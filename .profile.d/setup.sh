61dabc6496bf4f8da7603156e2e1287e3b68dddf
Rod Boev <rod.boev@gmail.com>
2024-09-15 00:40:47 -0400

    Try using pm2 for tunnel keep-alive

--- .profile.d/setup.sh
+++ .profile.d/setup.sh
@@ -38,12 +38,15 @@ chmod 700 /app/.ssh
 echo "$SSH_PRIVATE_KEY" > /app/.ssh/id_rsa
 chmod 600 /app/.ssh/id_rsa
 
-# Start the SSH tunnel in the background
-ssh -N -L $SSH_TUNNEL_FORWARD -i /app/.ssh/id_rsa -o StrictHostKeyChecking=no -p $SSH_TUNNEL_PORT $SSH_TUNNEL_TARGET &
-if [ $? -ne 0 ]; then
-    echo "Tunnel setup failed!"
-    exit 1
-fi
+# Write the command to a file to be executed by pm2
+echo "#!/bin/bash
+ssh -N -L $SSH_TUNNEL_FORWARD -i /app/.ssh/id_rsa -o StrictHostKeyChecking=no -p $SSH_TUNNEL_PORT $SSH_TUNNEL_TARGET
+" > /app/ssh_tunnel.sh
+chmod +x /app/ssh_tunnel.sh
+
+# Start the SSH tunnel using pm2
+pm2 start /app/ssh_tunnel.sh --name "ssh-tunnel"
+pm2 save
 
 echo "Tunnel setup successful."
 echo "setup.sh script completed"
