8da20a1cfd7c437cb42e2a553e55cd26ae182949
Rod Boev <rod.boev@gmail.com>
2024-10-19 13:30:48 -0400

    Revert back to all-in-one with pm2

--- .profile.d/setup.sh
+++ .profile.d/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting setup.sh"
+echo "Starting setup.sh script"
 
 # ODBC and FreeTDS Setup
 export ODBCSYSINI=/app/.apt/etc
@@ -31,47 +31,6 @@ EOL
 # Add FreeTDS bin to PATH
 export PATH=$PATH:/app/.apt/usr/bin
 
-# Function to check if a variable is set and print its value
-check_and_print_variable() {
-    if [ -z "${!1}" ]; then
-        echo "Warning: $1 is not set"
-    elif [[ "$1" == *"KEY"* ]]; then
-        echo "$1=${!1:0:10}..."
-    else
-        echo "$1=${!1}"
-    fi
-}
-
-# Check if folders and files exist
-echo "Checking if required folders and files exist:"
-
-folders_to_check=(
-    "/app/.apt/etc/freetds"
-    "$ODBCSYSINI"
-)
-
-files_to_check=(
-    "/app/.apt/etc/freetds/freetds.conf"
-    "$ODBCSYSINI/odbcinst.ini"
-    "$ODBCINI"
-)
-
-for folder in "${folders_to_check[@]}"; do
-    if [ -d "$folder" ]; then
-        echo "✅ Folder exists: $folder"
-    else
-        echo "❌ Folder does not exist: $folder"
-    fi
-done
-
-for file in "${files_to_check[@]}"; do
-    if [ -f "$file" ]; then
-        echo "✅ File exists: $file"
-    else
-        echo "❌ File does not exist: $file"
-    fi
-done
-
 # SSH Tunnel Setup
 echo "Setting up SSH tunnel..."
 mkdir -p /app/.ssh
@@ -79,22 +38,15 @@ chmod 700 /app/.ssh
 echo "$SSH_PRIVATE_KEY" > /app/.ssh/id_rsa
 chmod 600 /app/.ssh/id_rsa
 
-# Run the entire tunnel setup and management in the background
-(
-    if start_tunnel; then
-        while true; do
-            sleep 1800
-            echo "Restarting SSH tunnel..."
-            restart_tunnel
-        done
-    else
-        echo "Failed to set up initial tunnel. Exiting."
-        exit 1
-    fi
-) &
+# Write the command to a file to be executed by pm2
+echo "#!/bin/bash
+ssh -N -L $SSH_TUNNEL_FORWARD -i /app/.ssh/id_rsa -o StrictHostKeyChecking=no -p $SSH_TUNNEL_PORT $SSH_TUNNEL_TARGET
+" > /app/ssh_tunnel.sh
+chmod +x /app/ssh_tunnel.sh
 
-# Save the PID of the background process
-echo $! > ~/tunnel_manager.pid
-echo "Tunnel setup and restart mechanism initiated in background. Manager PID: $(cat ~/tunnel_manager.pid)"
+# Start the SSH tunnel using pm2
+pm2 start /app/ssh_tunnel.sh --name "ssh-tunnel"
+pm2 save
 
-echo "Finished setup.sh"
+echo "Tunnel setup successful."
+echo "setup.sh script completed"
