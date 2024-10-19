8061e4d7c7d22c09aa948f574ce2fc0144888494
Rod Boev <rod.boev@gmail.com>
2024-10-18 23:39:09 -0400

    House cleaning

--- scripts/dev-wrapper.sh
+++ scripts/dev-wrapper.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Set ODBCINI and FREETDSCONF environment variables
+export ODBCINI="$APPDATA\\FreeTDS\\odbc.ini"
+export FREETDSCONF="$APPDATA\\FreeTDS\\freetds.conf"
+
+# Run the tunnel setup script
 bash scripts/setupTunnelWindows.sh &
+
+# Start the Next.js development server
 npx next dev &
