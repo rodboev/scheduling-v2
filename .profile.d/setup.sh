c366b8d60afe319d5f8262884a8ee891d3355ac8
Rod Boev <rod.boev@gmail.com>
2024-10-19 01:22:51 -0400

    Revert to basic tunnels

---
+++ .profile.d/setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "Starting setup.sh script"
+
+# ODBC and FreeTDS Setup
+export ODBCSYSINI=/app/.apt/etc
+export ODBCINI=/app/.apt/etc/odbc.ini
+export FREETDSCONF=/app/.apt/etc/freetds/freetds.conf
+export LD_LIBRARY_PATH=/app/.apt/usr/lib/x86_64-linux-gnu:/app/.apt/usr/lib/x86_64-linux-gnu/odbc:$LD_LIBRARY_PATH
+
+mkdir -p /app/.apt/etc/freetds
+echo "[global]
+tds version = 7.4
+" > /app/.apt/etc/freetds/freetds.conf
+
+mkdir -p $ODBCSYSINI
+cat > "$ODBCSYSINI/odbcinst.ini" << EOL
+[FreeTDS]
+Description = FreeTDS Driver
+Driver = /app/.apt/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so
+Setup = /app/.apt/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so
+EOL
+
+cat > "$ODBCINI" << EOL
+[MSSQL]
+Driver = FreeTDS
+Server = 127.0.0.1
+Port = 1433
+Database = ${SQL_DATABASE}
+EOL
+
+# Add FreeTDS bin to PATH
+export PATH=$PATH:/app/.apt/usr/bin
+
+# SSH Tunnel Setup
+echo "Setting up SSH tunnel..."
+mkdir -p /app/.ssh
+chmod 700 /app/.ssh
+echo "$SSH_PRIVATE_KEY" > /app/.ssh/id_rsa
+chmod 600 /app/.ssh/id_rsa
+
+# Write the command to a file to be executed by pm2
+echo "#!/bin/bash
+ssh -N -L $SSH_TUNNEL_FORWARD -i /app/.ssh/id_rsa -o StrictHostKeyChecking=no -p $SSH_TUNNEL_PORT $SSH_TUNNEL_TARGET
+" > /app/ssh_tunnel.sh
+chmod +x /app/ssh_tunnel.sh
+
+# Start the SSH tunnel using pm2
+pm2 start /app/ssh_tunnel.sh --name "ssh-tunnel"
+pm2 save
+
+echo "Tunnel setup successful."
+echo "setup.sh script completed"
--- scripts/dev-wrapper.sh
+++ scripts/dev-wrapper.sh
@@ -4,8 +4,20 @@
 export ODBCINI="$APPDATA\\FreeTDS\\odbc.ini"
 export FREETDSCONF="$APPDATA\\FreeTDS\\freetds.conf"
 
-# Run the tunnel setup script
+# Function to clean up processes on exit
+cleanup() {
+    echo "Cleaning up Next.js process..."
+    pkill -f "next dev"
+    exit 0
+}
+
+# Set up trap to call cleanup function on script exit
+trap cleanup EXIT INT TERM
+
+# Run the tunnel setup script in the background
 bash scripts/setupTunnelWindows.sh &
 
 # Start the Next.js development server
-npx next dev &
+npx next dev --experimental-https
+
+# The script will wait here until Next.js exits or is interrupted
