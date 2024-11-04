2c25ad916beef934a9f2e72d36f217f381556a10
Rod Boev <rod.boev@gmail.com>
2024-11-04 05:51:29 -0500

    Fix MapMarkers

--- src/app/api/cluster/scheduling.js
+++ src/app/api/cluster/scheduling.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { chunk } from 'lodash'
+import { chunk } from '../../map/utils/array.js'
 import { calculateTravelTime } from '../../map/utils/distance.js'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || ''
---
+++ src/app/map/utils/array.js
@@ -0,0 +1,8 @@
+// New utility file for array operations
+export function chunk(array, size) {
+  const chunks = []
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size))
+  }
+  return chunks
+}
